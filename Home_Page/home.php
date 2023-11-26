383c8f5c9de580250dc3a93cef2aee0851470e5f
Muhammad Daffa Raihan <d.raihan2004@gmail.com>
2023-11-26 19:05:13 +0700

    membuat post blog

--- Home_Page/home.php
+++ Home_Page/home.php
@@ -15,7 +15,7 @@ if (isset($_SESSION['admin']))
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <title>E-Tourism</title>
-    <link rel="stylesheet" href="homestyle.css" />
+    <link rel="stylesheet" href="stylehome.css" />
   </head>
   <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary bg-gradient shadow-sm navbar sticky-top ">
@@ -88,7 +88,7 @@ while($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
 ?>
             <div class="col-md-4 mb-3">
             <a href="../Blog_Page/blog.php?id_blog=<?php echo $row['id_destinasi']; ?>" style="text-decoration: none; color: inherit;">
-              <div class="card border-top-0">
+              <div class="card border-top-0 card_show">
                 <img src="pict/<?php echo $row['gambar']?>" class="card-img-top" alt="Project 1">
                 <div class="card-body">
                   <h5 class="card-title"><?php echo  $row['judul']; ?></h5>
@@ -124,7 +124,7 @@ while($row = mysqli_fetch_array($result2, MYSQLI_ASSOC))
 ?>
           <div class="col-md-4 mb-3">
           <a href="../Show_Page/showpage.php?id_booking=<?php echo $row['id_booking']; ?>" style="text-decoration: none; color: inherit;">
-            <div class="card">
+            <div class="card card_show">
               <img src="pict/liburan1.png" class="card-img-top" alt="Project 1">
               <div class="card-body">
                 <h3 class="card-title"><?php echo $row['judul'] ?></h3>
