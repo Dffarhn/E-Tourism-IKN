a856b5b4275bdb1dfb0492b0c2eb121d3f47a223
Muhammad Daffa Raihan <d.raihan2004@gmail.com>
2023-11-28 07:39:00 +0700

    register alert

--- Registrasi_Page/register_p.php
+++ Registrasi_Page/register_p.php
@@ -5,14 +5,24 @@ include "../database/koneksi.php";
 
 $username = $_POST['username'];
 $password = $_POST['password'];
+$konfirmasi_password = $_POST['konfirmasi_password'];
 // $password = md5($password);
 
-$insertadmin = mysqli_query($conn, "INSERT INTO `admin` (`id_admin`, `username`, `password`) VALUES (NULL, '$username', MD5('$password'));");
-if ($insertadmin) {
-    echo '<META HTTP-EQUIV="Refresh" Content="0; URL=../Login_Page/Login.php">';
- 	exit;
-} else {
-    echo "Registrasi gagal. Coba lagi.";
+if ($password === $konfirmasi_password) {
+    # code...
+    $insertadmin = mysqli_query($conn, "INSERT INTO `admin` (`id_admin`, `username`, `password`) VALUES (NULL, '$username', MD5('$password'));");
+    if ($insertadmin) {
+        echo '<script>alert("Registration successful!");</script>';
+        echo '<META HTTP-EQUIV="Refresh" Content="0; URL=../Login_Page/Login.php">';
+         exit;
+    } else {
+        echo "Registrasi gagal. Coba lagi.";
+    }
+}else{
+    echo '<script>alert("Cek Password");</script>';
+    echo '<META HTTP-EQUIV="Refresh" Content="0; URL=../Registrasi_Page/registrasi.php">';
+
 }
+
 
 ?>
