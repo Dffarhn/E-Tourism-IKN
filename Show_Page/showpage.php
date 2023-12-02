9b8a01167e4c14507ce113ed70c8295278df072b
Muhammad Daffa Raihan <d.raihan2004@gmail.com>
2023-12-02 07:42:28 +0700

    hover cek tiket done

--- Show_Page/showpage.php
+++ Show_Page/showpage.php
@@ -21,7 +21,7 @@ if (isset($_GET['id_booking']) && isset($_SESSION['admin'])) {
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
       <title>E-Tourism</title>
-      <link rel="stylesheet" href="pageshow.css" />
+      <link rel="stylesheet" href="pageshow2.css" />
       <link rel="stylesheet" href="../asset/css card/image_card.css">
     </head>
     <body>
