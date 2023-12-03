6935343cc4cfb3b2a57bc52181ff7d9049745b51
Muhammad Daffa Raihan <d.raihan2004@gmail.com>
2023-12-03 23:49:55 +0700

    list database pesanan saya

--- profile_page/pesanansaya.php
+++ profile_page/pesanansaya.php
@@ -6,6 +6,8 @@ if (isset($_SESSION['admin']))
 {
   // echo $_SESSION['admin'];
 
+  $profile_now = $_SESSION['admin'];
+
   include "../database/koneksi.php";
 ?>
 
@@ -19,7 +21,7 @@ if (isset($_SESSION['admin']))
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <title>E-Tourism</title>
-    <link rel="stylesheet" href="profilestyle.css" />
+    <link rel="stylesheet" href="profilestylerr.css" />
   </head>
   <body>
 
@@ -33,6 +35,25 @@ if (isset($_SESSION['admin']))
     <div class="col-md-8 mt-5 px-5 content_profile">
         <div class="data_pribadi">
           <h3>Pesanan Saya</h3>
+
+<?php
+
+$result = mysqli_query($conn, "SELECT * ,d.judul FROM booking_history h JOIN bookingdestinasi d ON h.id_booking = d.id_booking WHERE h.id_admin = '$profile_now' ");
+while($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
+{
+?>
+          <div class="mt-4 list_pesanan px-3">
+            <div class="detail" >
+              
+              <h5><?php echo $row['judul'] ?></h5>
+              <p><?php echo $row['tanggal_pemesanan'] ?></p>
+              <p><?php echo $row['nama_pemesan'] ?>, Rp. <?php echo $row['harga_total'] ?></p>
+            </div>
+            <div class="but_place">
+              <button class="lihat_button" >Lihat</button>
+            </div>
+          </div>
+<?php }?>
 
         </div>
     </div>
--- profile_page/profile.php
+++ profile_page/profile.php
@@ -24,7 +24,7 @@ while($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <title>E-Tourism</title>
-    <link rel="stylesheet" href="profilestyler.css" />
+    <link rel="stylesheet" href="profilestylerr.css" />
   </head>
   <body>
     <!-- <nav class="navbar navbar-expand-lg navbar-dark bg-primary bg-gradient shadow-sm navbar sticky-top ">
